a02440f1e7f475648d47a9ede6a2d9244dc67d34
Carza-104 <carzaniga-github@proton.me>
2023-10-22 01:09:39 +0200

    Initial commit

---
+++ src/routes/page-controls/+page.svelte
@@ -0,0 +1,209 @@
+<script>
+	import { onMount } from 'svelte';
+	import {
+		Alert,
+		AlertButton,
+		AlertTextField,
+		NavigationBar,
+		NavigationBarLeading,
+		NavigationBarSearchField,
+		NavigationBarTrailing,
+		PageControl,
+		PageControlButton,
+		SegmentedControl,
+		SegmentedControlButton
+	} from 'apple-svelte';
+	import Code from '$lib/Code.svelte';
+	import { code } from './code';
+
+	let elementClass = 'default';
+
+	onMount(() => {
+		if (navigator.userAgent.includes('Win')) {
+			elementClass = 'windows';
+		}
+	});
+
+	let pageControlProps = [
+		{
+			name: 'showPlatter',
+			description: 'Insert description here.',
+			type: 'Boolean',
+			defaultValues: 'false\ntrue'
+		},
+		{
+			name: 'id',
+			description: 'Insert description here.',
+			type: 'String',
+			defaultValues: 'undefined'
+		},
+		{
+			name: 'style',
+			description: 'Insert description here.',
+			type: 'String',
+			defaultValues: 'undefined'
+		}
+	];
+
+	let pageControlButtonProps = [
+		{
+			name: 'inputGroup',
+			description: 'Insert description here.',
+			type: 'String',
+			defaultValues: '"page-control-button"'
+		},
+		{
+			name: 'state',
+			description: 'Insert description here.',
+			type: 'String',
+			defaultValues: '"default"\n"selected"'
+		},
+		{
+			name: 'id',
+			description: 'Insert description here.',
+			type: 'String',
+			defaultValues: 'undefined'
+		},
+		{
+			name: 'onPress',
+			description: 'Insert description here.',
+			type: 'Object',
+			defaultValues: 'undefined'
+		},
+		{
+			name: 'style',
+			description: 'Insert description here.',
+			type: 'String',
+			defaultValues: 'undefined'
+		}
+	];
+
+	let panel = 'preview';
+</script>
+
+<p>
+	Page control components display a row of pages in a list. You can have page control buttons call
+	any function when pressed, but it's recommended you use this to conditionally render content.
+</p>
+<SegmentedControl>
+	<SegmentedControlButton state="selected" label="Preview" onPress={() => (panel = 'preview')} />
+	<SegmentedControlButton label="Code" onPress={() => (panel = 'code')} />
+	<SegmentedControlButton label="Props" onPress={() => (panel = 'props')} />
+</SegmentedControl>
+{#if panel === 'preview'}
+	<PageControl showPlatter>
+		<PageControlButton state="selected" />
+		<PageControlButton />
+		<PageControlButton />
+	</PageControl>
+{:else if panel === 'code'}
+	<Code {code} />
+{:else}
+	<div class="table-container">
+		<code class="title3-emphasized">PageControl</code>
+		<div class="table {elementClass}">
+			<table>
+				<tr class="headline">
+					<td>Name</td>
+					<td>Type</td>
+					<td>Default values</td>
+				</tr>
+				{#each pageControlProps as prop}
+					<tr>
+						<td><code>{prop.name}</code></td>
+						<td>{prop.type}</td>
+						<td><code>{prop.defaultValues}</code></td>
+					</tr>
+				{/each}
+			</table>
+		</div>
+		<code class="title3-emphasized">PageControlButton</code>
+		<div class="table {elementClass}">
+			<table>
+				<tr class="headline">
+					<td>Name</td>
+					<td>Type</td>
+					<td>Default values</td>
+				</tr>
+				{#each pageControlButtonProps as prop}
+					<tr>
+						<td><code>{prop.name}</code></td>
+						<td>{prop.type}</td>
+						<td><code>{prop.defaultValues}</code></td>
+					</tr>
+				{/each}
+			</table>
+		</div>
+	</div>
+{/if}
+
+<style>
+	.alert {
+		backdrop-filter: blur(40px);
+		background: var(--materials-regular);
+		border-radius: 14px;
+		display: flex;
+		flex: 1;
+		flex-direction: column;
+		gap: 2px;
+		justify-content: center;
+		max-width: 270px;
+		overflow: hidden;
+		-webkit-backdrop-filter: blur(40px);
+	}
+
+	.title-and-description {
+		display: flex;
+		flex: 1;
+		flex-direction: column;
+		gap: 2px;
+		padding: 19px 16px 15px;
+	}
+
+	.alert-text {
+		text-align: center;
+	}
+
+	.table-container {
+		display: flex;
+		flex-direction: column;
+		gap: 24px;
+	}
+
+	.table {
+		background: var(--bg-grouped-secondary);
+		border-radius: 10px;
+		overflow-x: auto;
+	}
+
+	.windows::-webkit-scrollbar {
+		display: none;
+	}
+
+	table {
+		border-collapse: collapse;
+		white-space: pre;
+		width: 100%;
+	}
+
+	td {
+		border: 0.5px solid var(--separators-opaque);
+		padding: 11px 16px;
+	}
+
+	td:first-child {
+		border-left: unset;
+	}
+
+	td:last-child {
+		border-right: unset;
+	}
+
+	tr:first-child td {
+		border-top: unset;
+	}
+
+	tr:last-child td {
+		border-bottom: unset;
+	}
+</style>
