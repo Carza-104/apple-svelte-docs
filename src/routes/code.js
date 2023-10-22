8cc30e78f2e4fccdfb6dd5431cb32bbadcafdd4c
Carza-104 <carzaniga-github@proton.me>
2023-10-22 13:42:00 +0200

    Update code

--- src/routes/code.js
+++ src/routes/code.js
@@ -8,4 +8,5 @@ export let code = `<script>
         rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
     />
-</svelte:head>`;
+</svelte:head>
+`;
