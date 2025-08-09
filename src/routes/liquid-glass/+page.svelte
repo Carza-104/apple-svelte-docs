d5c33cb4d95d167b8f827858715206d7ab2ea877
Carza-104 <carzaniga-github@proton.me>
2025-08-09 20:33:35 +0200

    Initial commit

---
+++ src/routes/liquid-glass/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import {
+		Collection,
+		Version26List,
+		Version26ListRow,
+		Version26ListRowDefaultTrailing
+	} from 'apple-svelte';
+	import Code from '$lib/Code.svelte';
+	import { code } from './code';
+</script>
+
+<p>
+	To simulate Liquid Glass, apple-svelte applies background distortion on Blink browsers. To use
+	this in your SvelteKit project, you have to include the following code in your app.html file:
+</p>
+<Code title="app.html" {code} />
